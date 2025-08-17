ff7d68a2d607aab0c4985a46b8fd106b19020e6b
havi016 <havikampatel@outlook.com>
2025-08-17 17:43:24 +0100

    added authentication screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,22 @@
-import {View, Text, SafeAreaView} from 'react-native'
+import {View, Text, SafeAreaView, ImageBackground, KeyboardAvoidingView, Platform, Image, ScrollView, Dimensions} from 'react-native'
 import React from 'react'
 import {Slot} from "expo-router";
+import {images} from "@/constants";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
 
 export default function _Layout() {
     return (
-        <SafeAreaView>
-            <Text>Auth_Layout</Text>
-            <Slot />
-        </SafeAreaView>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
+            <ScrollView className={"bg-white h-full"} keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25}}>
+                    <ImageBackground source = {images.loginGraphic} className = "size-full rounded-b-lg" resizeMode={"stretch"} />
+                    <Image source={images.logo} className={"self-center size-48 absolute -bottom-16 z-10"}/>
+                </View>
+                <Slot />
+            </ScrollView>
+
+        </KeyboardAvoidingView>
     )
 }
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.havi.foodordering",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68a1f210000c1589f445',
+    userCollectionId: '68a1f2380026da7fc7b7',
+}
